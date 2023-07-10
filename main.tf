02c6d84963224458740e3cee173ad0c5caaf75af
Dennis <skatadennuchi@yahoo.com>
2023-07-10 17:21:21 -0500

    terrraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+
+resource "aws_sns_topic" "demo" {
+  name = var.topic_name
+  tags = {
+    team = var.Team
+  }
+}
+
+resource "aws_sns_topic_subscription" "email-target" {
+  topic_arn = aws_sns_topic.demo.arn
+  protocol  = var.protocoal_type
+  endpoint  = var.endpoint
+
+}
+
+
+
+
+
